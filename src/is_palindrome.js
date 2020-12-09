582ace6791b83fe3d5c9485373311c1c2db8f96a
Christabel Sebastian <christabel.escarez@gmail.com>
2020-12-09 14:46:07 -0800

    wrote isPalindrome

--- src/is_palindrome.js
+++ src/is_palindrome.js
@@ -1,6 +1,20 @@
-const isPalindrome = function (text) {
-
+const isPalindrome = function (text, first = 0, last = text.length - 1) {
+  if ((text.length <= 1) || (first >= last)) {
+    return true;
+  } else {
+    if (text[first] != text[last]) {
+      return false;
+    } else {
+      return isPalindrome(text, first + 1, last - 1);
+    }
+  }
 };
+
+console.log(isPalindrome(''));
+console.log(isPalindrome('a'));
+console.log(isPalindrome('racecar'));
+console.log(isPalindrome('raceca'));
+console.log(isPalindrome('asdf'));
 
 module.exports = isPalindrome;
 
